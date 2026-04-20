8f54465671cdb5513c0c48e12a2bb6c80fdc8cd8
danenberg15-star <danenberg15@gmail.com>
2026-04-20 01:02:18 -0700

    Enable Firestore offline persistence

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
@@ -14,6 +14,12 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+// הפעלת שמירה מקומית לאופליין (עבור טקסטים, קבוצות ומבנה הדוח)
+enableIndexedDbPersistence(db).catch((err) => {
+  console.error("Offline persistence error:", err.code);
+});
+
 export const storage = getStorage(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
